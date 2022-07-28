8e6e0c0dd169b5b03a636ddb3b8f19bea3dfdf7d
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-07-28 10:35:26 -0500

    Refactor: Ajuste form y quitar reservaciones

--- js/contac.js
+++ js/contac.js
@@ -1,9 +1,7 @@
-const sendNotification = (name, apellido, phone, mensaje, fecha, tiempo) =>  {
-  const message = `Hola! \nQuisiera reservar una mesa con ustedes:
+const sendNotification = (name, apellido, phone, mensaje) =>  {
+  const message = `Hola! \nQuisiera comunicarme con ustedes:
                         Mi nombre es: ${name}
-                        Apellido: ${apellido}
-                        Fecha: ${fecha}
-                        Tiempo: ${tiempo}
+                        Apellidos: ${apellido}
                         Teléfono: ${phone}
                         Mensaje: ${mensaje}`.replace(/  +/g, '');
 
@@ -13,12 +11,10 @@ const sendNotification = (name, apellido, phone, mensaje, fecha, tiempo) =>  {
 $( "#button-send-contact" ).click(function() {
   const name= $("#name").val();
   const apellido= $("#apellido").val();
-  const fecha= $("#fecha").val();
-  const tiempo= $("#tiempo").val();
   const phone= $("#phone").val();
   const mensaje= $("#mensaje").val();
-  if(name.length > 0, apellido.length >0, fecha.length > 0, tiempo.length > 0, phone.length >0, mensaje.length >0){
-    sendNotification(name, apellido, phone, mensaje, fecha, tiempo);
+  if(name.length > 0, apellido.length > 0, phone.length > 0, mensaje.length > 0){
+    sendNotification(name, apellido, phone, mensaje);
   }
   console.log(this)
 });
