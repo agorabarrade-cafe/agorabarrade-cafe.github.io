e940d874f4f21bc643d58db53e3f62f1744a9d41
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-07-28 21:24:58 -0500

    Refactor: Ajuste en espacios, form y enlaces

--- js/contac.js
+++ js/contac.js
@@ -1,20 +1,14 @@
-const sendNotification = (name, apellido, phone, mensaje) =>  {
-  const message = `Hola! \nQuisiera comunicarme con ustedes:
-                        Mi nombre es: ${name}
-                        Apellidos: ${apellido}
-                        Teléfono: ${phone}
-                        Mensaje: ${mensaje}`.replace(/  +/g, '');
-
+const sendNotification = (name, apellido, mensaje) =>  {
+  const message = `¡Hola! Soy ${name} ${apellido}, estoy interesad@ en: ${mensaje}`.replace(/  +/g, '');
   window.open(`https://api.whatsapp.com/send?phone=525591703575&text=${encodeURIComponent(message)}`)
 }
 
 $( "#button-send-contact" ).click(function() {
   const name= $("#name").val();
   const apellido= $("#apellido").val();
-  const phone= $("#phone").val();
   const mensaje= $("#mensaje").val();
-  if(name.length > 0, apellido.length > 0, phone.length > 0, mensaje.length > 0){
-    sendNotification(name, apellido, phone, mensaje);
+  if(name.length > 0, apellido.length > 0, mensaje.length > 0){
+    sendNotification(name, apellido, mensaje);
   }
   console.log(this)
 });
